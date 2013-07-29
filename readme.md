5d166c5b21b03c998e4ea2305d4e7621514d0419
Dimitris Verraros <vervas.g@gmail.com>
2013-07-29 16:58:49 +0200

    Update, refactor and rebrand

--- readme.md
+++ readme.md
@@ -2,22 +2,10 @@
 
 This not a buildpack. These are common functions related to
 JVM buildpacks. It is formatted as a buildpack in order to
-make use of [heroku-buildpack-testrunner](http://github.com/ryanbrainard/heroku-buildpack-testrunner)
+make use of [heroku-buildpack-testrunner](http://github.com/cloudControl/heroku-buildpack-testrunner)
 test automation.
 
 The most important function these scripts provide are functions
 to install the JDK into a at compile time.
 
-These scripts are bundled and stored in a tar ball at:
-
-http://heroku-jvm-common.s3.amazonaws.com/jvm-buildpack-common.tar.gz
-
-#Example Usage
-
-```
-curl --silent --location http://heroku-jvm-common.s3.amazonaws.com/jvm-buildpack-common.tar.gz | tar xz
-
-. bin/java
-
-install_java ${BUILD_DIR} "1.7"
-```
+These scripts are bundled and stored in a tarball.
--- test/java_test.sh
+++ test/java_test.sh
@@ -4,43 +4,27 @@
 . ${BUILDPACK_HOME}/bin/java
 . ${BUILDPACK_HOME}/test/testlib
 
+testOverlayDir() {
+  mkdir -p ${BUILD_DIR}/.jdk/jre/bin
+  touch ${BUILD_DIR}/.jdk/jre/bin/java 
+  mkdir -p ${BUILD_DIR}/.jdk-overlay/jre/lib/security
+  mkdir -p ${BUILD_DIR}/.jdk-overlay/jre/bin
+  touch ${BUILD_DIR}/.jdk-overlay/jre/lib/security/policy.jar
+  capture jdk_overlay ${BUILD_DIR}
+  assertTrue "Files in .jdk-overlay should be copied to .jdk." "[ -f ${BUILD_DIR}/.jdk/jre/lib/security/policy.jar ]"
+  assertTrue "Files in .jdk should not be overwritten." "[ -f ${BUILD_DIR}/.jdk/jre/bin/java ]"
+}
+
 testDetectJava() {
   echo "java.runtime.version=1.8" >> ${BUILD_DIR}/system.properties
   capture detect_java_version ${BUILD_DIR}
   assertCapturedEquals "1.8"
 }
 
-testUserIsFlaggedForDownload() {
-  capture is_flagged_for_download 100
-  assertCapturedEquals "true"
-}
-
-testUserIsNotFlaggedForDownload() {
-  capture is_flagged_for_download 0
-  assertCapturedEquals "false"
-}
-
-testInvalidInputForFlag() {
-  capture is_flagged_for_download "<somexmlerror>err</somexmlerr>"
-  assertCapturedEquals "false"
-}
-
-testFlagIsDownloaded() {
-  capture is_flagged_for_download
-  flagged=$(cat $STD_OUT)
-  assertTrue "Response should be true or false." "[ $flagged = 'true' ] || [ $flagged = 'false' ]"
-}
-
 test_defaultJdkUrl() {
   capture _get_jdk_download_url "${DEFAULT_JDK_VERSION}"
   assertCapturedSuccess
-  assertTrue "The URL should be for the default JDK, ${DEFAULT_JDK_VERSION}." "[ $(cat ${STD_OUT}) = '${JDK_URL_1_6}' ] || [ '$(cat ${STD_OUT})' = '${JDK_URL_1_6_DARWIN}' ]"
-}
-
-test_nonDefaultJdkUrl() {
-  capture _get_jdk_download_url "${LATEST_JDK_VERSION}"
-  assertCapturedSuccess
-  assertTrue "The URL should be for the latest JDK, ${LATEST_JDK_VERSION}." "[ $(cat ${STD_OUT}) = '${JDK_URL_1_7}' ] || [ '$(cat ${STD_OUT})' = '${JDK_URL_1_7_DARWIN}' ]"
+  assertTrue "The URL should be for the default JDK, ${DEFAULT_JDK_VERSION}." "[ $(cat ${STD_OUT}) = '${JDK_URL_1_6}' ]"
 }
 
 test_installJavaWithoutDirectoryFails() {
@@ -108,4 +92,31 @@ test_installJavaWith1_5() {
   assertCapturedSuccess
   assertTrue "Precondition: JDK6 should have been installed." "[ $(cat ${BUILD_DIR}/.jdk/version) = '${DEFAULT_JDK_VERSION}' ]"
   assertEquals "${BUILD_DIR}/.jdk/$(_get_relative_jdk_bin)/java" "$(which java)"
+}
+
+test_nonEmptyCacert1_6(){
+  unset JAVA_HOME
+  capture install_java ${BUILD_DIR} "1.6"
+  assertCapturedSuccess
+
+  CACERTS_COUNT="$(${BUILD_DIR}/.jdk/$(_get_relative_jdk_bin)/keytool -list -v -keystore ${BUILD_DIR}/.jdk/jre/lib/security/cacerts -storepass changeit | grep "Alias name" | wc -l)"
+  assertTrue "Cacert file contains less than 100 domains" "[ $CACERTS_COUNT -gt "100" ]"
+}
+
+test_nonEmptyCacert1_7(){
+  unset JAVA_HOME
+  capture install_java ${BUILD_DIR} "1.7"
+  assertCapturedSuccess
+
+  CACERTS_COUNT="$(${BUILD_DIR}/.jdk/$(_get_relative_jdk_bin)/keytool -list -v -keystore ${BUILD_DIR}/.jdk/jre/lib/security/cacerts -storepass changeit | grep "Alias name" | wc -l)"
+  assertTrue "Cacert file contains less than 100 domains" "[ $CACERTS_COUNT -gt "100" ]"
+}
+
+test_nonEmptyCacert1_8(){
+  unset JAVA_HOME
+  capture install_java ${BUILD_DIR} "1.8"
+  assertCapturedSuccess
+
+  CACERTS_COUNT="$(${BUILD_DIR}/.jdk/$(_get_relative_jdk_bin)/keytool -list -v -keystore ${BUILD_DIR}/.jdk/jre/lib/security/cacerts -storepass changeit | grep "Alias name" | wc -l)"
+  assertTrue "Cacert file contains less than 100 domains" "[ $CACERTS_COUNT -gt "100" ]"
 }
