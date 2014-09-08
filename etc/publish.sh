cb3cc824cb6b055d1ce8111161d085eba8e50a40
Joe Kutner <jpkutner@gmail.com>
2014-09-08 15:47:49 -0500

    fixed some problems with the publish script

--- etc/publish.sh
+++ etc/publish.sh
@@ -5,21 +5,22 @@ set -o pipefail
 # fail harder
 set -eu
 
-if [ "hot" = "$1" ]; then
+if [ "hot" = "${1:-not}" ]; then
   echo "---> Preparing repo. Using current directory."
   SNAPSHOT="-SNAPSHOT"
 else
   echo "---> Preparing repo. Using master branch."
   pushd . > /dev/null 2>&1
   cd /tmp
-  git clone git@github.com:heroku/heroku-buildpack-scala.git
-  cd heroku-buildpack-scala
+  rm -rf heroku-buildpack-jvm-common
+  git clone git@github.com:heroku/heroku-buildpack-jvm-common.git
+  cd heroku-buildpack-jvm-common
   git checkout master
   find . ! -name 'bin' ! -name 'version.properties' -maxdepth 1 -delete
 fi
 
 . version.properties
-RELEASE_FILE="jvm-buildpack-common-v${VERSION}${SNAPSHOT}.tar.gz"
+RELEASE_FILE="jvm-buildpack-common-v${VERSION}${SNAPSHOT:=}.tar.gz"
 
 echo -n "---> Checking uniqueness of version..."
 S3_FILE=$(aws s3 ls s3://lang-jvm/$RELEASE_FILE --profile lang-jvm)
