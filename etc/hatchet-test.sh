130953ed348b62fdccafa1f364fe115ecb9e3a39
Joe Kutner <jpkutner@gmail.com>
2015-01-12 13:19:11 -0600

    Removed some unneeded lines from hatchet test script

--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -1,6 +1,4 @@
 hatchet install &&
 HATCHET_RETRIES=3 \
 HATCHET_DEPLOY_STRATEGY=git \
-HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-jvm-common.git" \
-HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
 rspec $@
