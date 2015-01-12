5ccb3ebb9ac0f623f572b7e8be8d7ad94ab3db45
Joe Kutner <jpkutner@gmail.com>
2015-01-12 11:26:25 -0600

    Fixed a bug in initial hatchet spec

--- spec/java_spec.rb
+++ spec/java_spec.rb
@@ -5,7 +5,7 @@ describe "Java" do
   before(:each) do
     set_java_version(app.directory, jdk_version)
     app.setup!
-    app.set_config("JVM_COMMON_BUILDPACK", "https://github.com/heroku/heroku-buildpack-jvm-common/tarball/#{`git rev-parse HEAD`}")
+    app.set_config("JVM_COMMON_BUILDPACK" => "https://github.com/heroku/heroku-buildpack-jvm-common/tarball/#{`git rev-parse HEAD`}")
     app.heroku.put_stack(app.name, "cedar-14")
   end
 
@@ -14,7 +14,7 @@ describe "Java" do
       let(:app) { Hatchet::Runner.new("java-servlets-sample") }
       let(:jdk_version) { version }
       it "should not install settings.xml" do
-        app.set_config("BUILDPACK_URL", "https://github.com/heroku/heroku-buildpack-java#upgrade-jvm-common")
+        app.set_config("BUILDPACK_URL" => "https://github.com/heroku/heroku-buildpack-java#upgrade-jvm-common")
         app.deploy do |app|
           expect(app.output).to include("Installing OpenJDK #{jdk_version}")
           expect(successful_body(app)).to eq("Hello from Java!")
