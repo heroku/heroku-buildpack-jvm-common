7c649d2e2d67565cfb9c63d8f86bf836bb20db01
Joe Kutner <jpkutner@gmail.com>
2015-01-12 12:52:21 -0600

    Set the default buildpack url for the java sample to the master branch of the java buildpack repo

--- spec/java_spec.rb
+++ spec/java_spec.rb
@@ -6,19 +6,16 @@ describe "Java" do
     set_java_version(app.directory, jdk_version)
     app.setup!
     app.set_config("JVM_COMMON_BUILDPACK" => "https://github.com/heroku/heroku-buildpack-jvm-common/tarball/#{`git rev-parse HEAD`}")
-    app.heroku.put_stack(app.name, "cedar-14")
   end
 
   ["1.7", "1.8"].each do |version|
     context "on jdk-#{version}" do
-      let(:app) { Hatchet::Runner.new("java-servlets-sample") }
+      let(:app) { Hatchet::Runner.new("java-servlets-sample",
+        :buildpack_url => "https://github.com/heroku/heroku-buildpack-java") }
       let(:jdk_version) { version }
-      it "should not install settings.xml" do
-        app.set_config("BUILDPACK_URL" => "https://github.com/heroku/heroku-buildpack-java#upgrade-jvm-common")
+      it "should deploy a simple java app" do
         app.deploy do |app|
           expect(app.output).to include("Installing OpenJDK #{jdk_version}")
-          expect(successful_body(app)).to eq("Hello from Java!")
-          expect(app.output).not_to include("Installing Maven")
           expect(app.output).to include("BUILD SUCCESS")
           expect(successful_body(app)).to eq("Hello from Java!")
         end
