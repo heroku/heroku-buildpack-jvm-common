becbea57a6f1b6d647d3bb79e29e04886ed1db17
Joe Kutner <jpkutner@gmail.com>
2015-01-20 11:27:50 -0600

    Replaced publish script with buildkit publish script

--- etc/publish.sh
+++ etc/publish.sh
@@ -1,55 +1,38 @@
 #!/bin/bash
 
-# fail hard
-#set -o pipefail
+set -e
 
-# fail harder
-#set -e
+BP_NAME="jvm-common"
 
-if [ "hot" = "${1:-}" ]; then
-  echo "---> Preparing repo. Using current directory."
-  SNAPSHOT="-SNAPSHOT"
-else
-  echo "---> Preparing repo. Using master branch."
+if [ ! -z "$1" ]; then
   pushd . > /dev/null 2>&1
-  cd /tmp
-  rm -rf heroku-buildpack-jvm-common
-  git clone git@github.com:heroku/heroku-buildpack-jvm-common.git
-  cd heroku-buildpack-jvm-common
+  cd /tmp &&
+  git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
+  cd heroku-buildpack-$BP_NAME
   git checkout master
-  find . ! -name '.' ! -name '..' ! -name 'version.properties' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf --
-fi
+  headHash=$(git rev-parse HEAD)
 
-. version.properties
-RELEASE_FILE="jvm-buildpack-common-v${VERSION}${SNAPSHOT:=}.tar.gz"
+  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf --
+  heroku buildpacks:publish $1/$BP_NAME
 
-echo "---> Checking uniqueness of version..."
-S3_FILE=$(aws s3 ls s3://lang-jvm/$RELEASE_FILE --profile lang-jvm)
-if [ -z "$SNAPSHOT" ] && [ -n "$S3_FILE" ]; then
-  echo ""
-  echo ${S3_FILE}
-  echo "A file '$RELEASE_FILE' already exists!"
+  if [ "$1" = "heroku" ]; then
+    newTag=$(heroku buildpacks:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
+  fi
+
+  popd > /dev/null 2>&1
+  echo "Cleaning up..."
+  rm -rf /tmp/heroku-buildpack-$BP_NAME
+
+  if [ "$1" = "heroku" ]; then
+    if [ "$headHash" = "$(git rev-parse HEAD)" ]; then
+      echo "Tagging commit $headHash with $newTag... "
+      git tag $newTag
+      git push --tags
+    fi
+  fi
+
+  echo "Done."
+else
+  echo "You must provide a buildkit organization as an argument!"
   exit 1
 fi
-
-echo "---> Creating archive..."
-tar pczf ../$RELEASE_FILE .
-mv ../$RELEASE_FILE .
-
-echo "---> Uploading to S3..."
-aws s3 cp $RELEASE_FILE s3://lang-jvm --profile lang-jvm --acl public-read
-
-if [ -z "$SNAPSHOT" ]; then
-  popd > /dev/null 2>&1
-
-  echo "---> Cleaning up..."
-  rm -rf /tmp/heroku-buildpack-jvm-common
-
-  echo "---> Tagging release..."
-  git tag v${VERSION}
-  git push --tags origin master
-
-  echo "VERSION=\"$(($VERSION + 1))\"" > version.properties
-fi
-
-echo "---> Done."
