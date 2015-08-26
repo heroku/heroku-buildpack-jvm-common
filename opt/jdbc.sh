73e0361c6fc1be6502fad55dc4609385d2128bc2
Joe Kutner <jpkutner@gmail.com>
2015-08-26 10:46:41 -0500

    Refactored JDBC url. Add user/pass as URL params

--- opt/jdbc.sh
+++ opt/jdbc.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
-if [ -n "$DATABASE_URL" ]; then
-  db_protocol=$(expr "$DATABASE_URL" : "\(.\+\)://")
+set_jdbc_url() {
+  local db_url=${1}
+
+  db_protocol=$(expr "$db_url" : "\(.\+\)://")
   if [ $db_protocol == "postgres" ]; then
     jdbc_protocol="jdbc:postgresql"
   elif [ $db_protocol == "mysql" ]; then
     jdbc_protocol="jdbc:mysql"
   fi
 
-  db_user=$(expr "$DATABASE_URL" : "${db_protocol}://\(.\+\):\(.\+\)@")
+  db_user=$(expr "$db_url" : "${db_protocol}://\(.\+\):\(.\+\)@")
   db_prefix="${db_protocol}://${db_user}:"
 
-  db_pass=$(expr "$DATABASE_URL" : "${db_prefix}\(.\+\)@")
+  db_pass=$(expr "$db_url" : "${db_prefix}\(.\+\)@")
   db_prefix="${db_prefix}${db_pass}@"
 
-  db_host_port=$(expr "$DATABASE_URL" : "${db_prefix}\(.\+\)/")
+  db_host_port=$(expr "$db_url" : "${db_prefix}\(.\+\)/")
   db_prefix="${db_prefix}${db_host_port}/"
 
-  db_name_args=$(expr "$DATABASE_URL" : "${db_prefix}\(.\+\)")
+  db_name=$(expr "$db_url" : "${db_prefix}\(.\+\)")
 
-  export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_name_args}"
+  export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_name}?user=${db_user}&password=${db_pass}"
   export JDBC_DATABASE_USERNAME="${db_user}"
   export JDBC_DATABASE_PASSWORD="${db_pass}"
+}
+
+if [ -n "$DATABASE_URL" ]; then
+  set_jdbc_url "$DATABASE_URL"
+elif [ -n "$JAWSDB_URL" ]; then
+  set_jdbc_url "$JAWSDB_URL"
+elif [ -n "$CLEARDB_DATABASE_URL" ]; then
+  set_jdbc_url "$CLEARDB_DATABASE_URL"
 fi
