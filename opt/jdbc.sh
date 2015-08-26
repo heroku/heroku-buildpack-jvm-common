d8e6560dd960ae50badeba51e195aa4191ffa369
Joe Kutner <jpkutner@gmail.com>
2015-08-26 11:20:01 -0500

    Handle case where db url does not match pattern

--- opt/jdbc.sh
+++ opt/jdbc.sh
@@ -4,26 +4,28 @@ set_jdbc_url() {
   local db_url=${1}
 
   db_protocol=$(expr "$db_url" : "\(.\+\)://")
-  if [ $db_protocol == "postgres" ]; then
+  if [ "$db_protocol" == "postgres" ]; then
     jdbc_protocol="jdbc:postgresql"
-  else
-    jdbc_protocol="jdbc:${db_protocol}"
+  elif [ "$db_protocol" == "mysql" ]; then
+    jdbc_protocol="jdbc:mysql"
   fi
 
-  db_user=$(expr "$db_url" : "${db_protocol}://\(.\+\):\(.\+\)@")
-  db_prefix="${db_protocol}://${db_user}:"
+  if [ -n "$jdbc_protocol" ]; then
+    db_user=$(expr "$db_url" : "${db_protocol}://\(.\+\):\(.\+\)@")
+    db_prefix="${db_protocol}://${db_user}:"
 
-  db_pass=$(expr "$db_url" : "${db_prefix}\(.\+\)@")
-  db_prefix="${db_prefix}${db_pass}@"
+    db_pass=$(expr "$db_url" : "${db_prefix}\(.\+\)@")
+    db_prefix="${db_prefix}${db_pass}@"
 
-  db_host_port=$(expr "$db_url" : "${db_prefix}\(.\+\)/")
-  db_prefix="${db_prefix}${db_host_port}/"
+    db_host_port=$(expr "$db_url" : "${db_prefix}\(.\+\)/")
+    db_prefix="${db_prefix}${db_host_port}/"
 
-  db_name=$(expr "$db_url" : "${db_prefix}\(.\+\)")
+    db_name=$(expr "$db_url" : "${db_prefix}\(.\+\)")
 
-  export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_name}?user=${db_user}&password=${db_pass}"
-  export JDBC_DATABASE_USERNAME="${db_user}"
-  export JDBC_DATABASE_PASSWORD="${db_pass}"
+    export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_name}?user=${db_user}&password=${db_pass}"
+    export JDBC_DATABASE_USERNAME="${db_user}"
+    export JDBC_DATABASE_PASSWORD="${db_pass}"
+  fi
 }
 
 if [ -n "$DATABASE_URL" ]; then
