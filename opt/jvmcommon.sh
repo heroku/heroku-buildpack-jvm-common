abe0c06ca992345e848aef24fd901d87a53f18bc
Joe Kutner <jpkutner@gmail.com>
2015-08-27 10:06:02 -0500

    Refactored JAVA_TOOL_OPTIONS

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -5,26 +5,34 @@ export PATH="$HOME/.heroku/bin:$JAVA_HOME/bin:$PATH"
 limit=$(ulimit -u)
 case $limit in
 256)   # 1X Dyno
-  default_java_opts="-Xmx384m -Xss512k"
+  default_java_mem_opts="-Xmx384m -Xss512k"
   ;;
 512)   # 2X Dyno
-  default_java_opts="-Xmx768m"
+  default_java_mem_opts="-Xmx768m"
   ;;
 16384) # IX Dyno
-  default_java_opts="-Xmx2g"
+  default_java_mem_opts="-Xmx2g"
   ;;
 32768) # PX Dyno
-  default_java_opts="-Xmx12g"
+  default_java_mem_opts="-Xmx12g"
   ;;
 *)
-  default_java_opts="-Xmx384m -Xss512k"
+  default_java_mem_opts="-Xmx384m -Xss512k"
   ;;
 esac
 
-export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"${default_java_opts} -Dfile.encoding=UTF-8"}
-
 if [[ "${JAVA_OPTS}" == *-Xmx* ]]; then
-  export JAVA_OPTS="$JAVA_OPTS"
+  export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
+elif [[ "${JVM_OPTS}" == *-Xmx* ]]; then
+  export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
 else
+  default_java_opts="${default_java_mem_opts} -Dfile.encoding=UTF-8"
+  export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-${default_java_opts}}
   export JAVA_OPTS="${default_java_opts} $JAVA_OPTS"
+  if [[ "${DYNO}" != *run.* ]]; then
+    export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-${default_java_opts}}
+  fi
+  if [[ "${DYNO}" == *web.* ]]; then
+    echo "Setting JAVA_TOOL_OPTIONS defaults for dyno size. Custom settings will override it."
+  fi
 fi
