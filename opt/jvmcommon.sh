abb6ff6b821d65ee64a07fcb47d5917cfcc5b4be
Joe Kutner <jpkutner@gmail.com>
2015-09-02 17:45:55 -0500

    Always set JVM_OPTS with memory defaults

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -21,12 +21,12 @@ case $limit in
   ;;
 esac
 
+default_java_opts="${default_java_mem_opts} -Dfile.encoding=UTF-8"
+export JVM_OPTS="${default_java_opts}"
+
 if [[ "${JAVA_OPTS}" == *-Xmx* ]]; then
   export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
-elif [[ "${JVM_OPTS}" == *-Xmx* ]]; then
-  export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
 else
-  default_java_opts="${default_java_mem_opts} -Dfile.encoding=UTF-8"
   export JAVA_OPTS="${default_java_opts} $JAVA_OPTS"
   if [[ "${DYNO}" != *run.* ]]; then
     export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-${default_java_opts}}
