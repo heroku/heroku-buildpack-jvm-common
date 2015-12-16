ab9f95c5355a54605678103b4236ff06e0699ee7
Joe Kutner <jpkutner@gmail.com>
2015-12-16 08:46:40 -0600

    Fix formatting

--- opt/jdbc.sh
+++ opt/jdbc.sh
@@ -6,32 +6,32 @@ set_jdbc_url() {
   if [ -z "$JDBC_DATABASE_URL" ]; then
       local db_protocol=$(expr "$db_url" : "\(.\+\)://")
       if [ "$db_protocol" == "postgres" ]; then
-	  local jdbc_protocol="jdbc:postgresql"
+        local jdbc_protocol="jdbc:postgresql"
       elif [ "$db_protocol" == "mysql" ]; then
-	  local jdbc_protocol="jdbc:mysql"
+        local jdbc_protocol="jdbc:mysql"
       fi
 
       if [ -n "$jdbc_protocol" ]; then
-	  local db_user=$(expr "$db_url" : "${db_protocol}://\(.\+\):\(.\+\)@")
-	  local db_prefix="${db_protocol}://${db_user}:"
+        local db_user=$(expr "$db_url" : "${db_protocol}://\(.\+\):\(.\+\)@")
+        local db_prefix="${db_protocol}://${db_user}:"
 
-	  local db_pass=$(expr "$db_url" : "${db_prefix}\(.\+\)@")
-	  db_prefix="${db_prefix}${db_pass}@"
+        local db_pass=$(expr "$db_url" : "${db_prefix}\(.\+\)@")
+        db_prefix="${db_prefix}${db_pass}@"
 
-	  local db_host_port=$(expr "$db_url" : "${db_prefix}\(.\+\)/")
-	  db_prefix="${db_prefix}${db_host_port}/"
+        local db_host_port=$(expr "$db_url" : "${db_prefix}\(.\+\)/")
+        db_prefix="${db_prefix}${db_host_port}/"
 
-	  local db_suffix=$(expr "$db_url" : "${db_prefix}\(.\+\)")
+        local db_suffix=$(expr "$db_url" : "${db_prefix}\(.\+\)")
 
-          if [[ "$db_suffix" == *\?* ]]; then
-            local db_args="&user=${db_user}&password=${db_pass}"
-	  else
-            local db_args="?user=${db_user}&password=${db_pass}"
-	  fi
+        if [[ "$db_suffix" == *\?* ]]; then
+          local db_args="&user=${db_user}&password=${db_pass}"
+        else
+          local db_args="?user=${db_user}&password=${db_pass}"
+        fi
 
-	  export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_suffix}${db_args}"
-	  export JDBC_DATABASE_USERNAME="${db_user}"
-	  export JDBC_DATABASE_PASSWORD="${db_pass}"
+        export JDBC_DATABASE_URL="${jdbc_protocol}://${db_host_port}/${db_suffix}${db_args}"
+        export JDBC_DATABASE_USERNAME="${db_user}"
+        export JDBC_DATABASE_PASSWORD="${db_pass}"
       fi
   fi
 }
