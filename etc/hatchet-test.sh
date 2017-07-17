8f0fdd1fb3c9c35d95d09c49df6720270e4b39bb
Joe Kutner <jpkutner@gmail.com>
2017-07-17 07:51:27 -1000

    Updated travis ci config

--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-gem install rspec-retry -v 0.4.0
-#gem install heroku_hatchet -v 1.4.1
+if [ -z "$HEROKU_API_KEY" ]; then
+  echo "Missing \$HEROKU_API_KEY. Skipping."
+  exit 0
+elif [ -n "$CIRCLE_BRANCH" ]; then
+  export HATCHET_BUILDPACK_BRANCH="$CIRCLE_BRANCH"
+elif [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+  export HATCHET_BUILDPACK_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+else
+  export HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#')
+fi
 
-hatchet install &&
-HEROKU_APP_LIMIT=9999 \
+gem install bundler
+bundle install
+
+bundle exec hatchet install &&
 HATCHET_RETRIES=3 \
 HATCHET_DEPLOY_STRATEGY=git \
 HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-jvm-common.git" \
-HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
-rspec $@
+bundle exec rspec $@
