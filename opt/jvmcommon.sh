11ebd590ac4338be20ec7121655f6cb29aa605d7
BuildpackTester <jpkutner@gmail.com>
2017-08-24 08:28:24 -0500

    Merge existing JAVA_TOOL_OPTIONS with default JAVA_TOOL_OPTIONS instead of skipping

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -27,7 +27,7 @@ else
   default_java_opts="${default_java_mem_opts} -Dfile.encoding=UTF-8"
   export JAVA_OPTS="${default_java_opts} $JAVA_OPTS"
   if [[ "${DYNO}" != *run.* ]]; then
-    export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-${default_java_opts}}
+    export JAVA_TOOL_OPTIONS="${default_java_opts} $JAVA_TOOL_OPTIONS"
   fi
   if [[ "${DYNO}" == *web.* ]]; then
     echo "Setting JAVA_TOOL_OPTIONS defaults based on dyno size. Custom settings will override them."
