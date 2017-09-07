7d6e96076b0f41d6ca016ac76c9f08ffb59b9578
BuildpackTester <jpkutner@gmail.com>
2017-09-07 14:16:52 -0500

    Updated hatchet test script

--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -1,9 +1,28 @@
-#!/usr/bin/env bash
+
+set -e
+
+BUILDPACK_NAME="heroku-buildpack-java"
+
+if [ "$CIRCLECI" == "true" ] && [ -n "$CI_PULL_REQUEST" ]; then
+  if [ "$CIRCLE_PR_USERNAME" != "heroku" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
+
+if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  if [ "$TRAVIS_PULL_REQUEST_SLUG" != "heroku/$BUILDPACK_NAME" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
 
 if [ -z "$HEROKU_API_KEY" ]; then
-  echo "Missing \$HEROKU_API_KEY. Skipping."
-  exit 0
-elif [ -n "$CIRCLE_BRANCH" ]; then
+  echo "ERROR: Missing \$HEROKU_API_KEY."
+  exit 1
+fi
+
+if [ -n "$CIRCLE_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$CIRCLE_BRANCH"
 elif [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
@@ -13,9 +32,11 @@ fi
 
 gem install bundler
 bundle install
+bundle exec hatchet install
 
-bundle exec hatchet install &&
-HATCHET_RETRIES=3 \
-HATCHET_DEPLOY_STRATEGY=git \
-HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-jvm-common.git" \
+export HATCHET_RETRIES=3
+export HATCHET_APP_LIMIT=20
+export HATCHET_DEPLOY_STRATEGY=git
+export HATCHET_BUILDPACK_BASE="https://github.com/heroku/$BUILDPACK_NAME"
+
 bundle exec rspec $@
