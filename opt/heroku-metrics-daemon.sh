d08f45250ef3b88a095e2314a3019957213c01ae
BuildpackTester <jpkutner@gmail.com>
2017-11-20 08:33:20 -0600

    Added function to install heroku-metrics-agent and test

---
+++ opt/heroku-metrics-daemon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# TODO set -eu
+
+# don't do anything if we don't have a metrics url.
+if [[ -z "${HEROKU_METRICS_URL:-}" ]] || [[ "${DYNO}" = run\.* ]]; then
+    return 0
+fi
+
+# heroku-metrics-agent.jar is added in bin/compile
+if [[ -f ${HOME}/.heroku/bin/heroku-metrics-agent.jar ]] && [[ -z "${DISABLE_HEROKU_METRICS_AGENT:-}" ]]; then
+  if [[ -f build.sbt ]] || # Scala
+     [[ -d target/resolution-cache ]]; then # Scala (sbt-heroku)
+    export JAVA_OPTS="-javaagent:${HOME}/.heroku/bin/heroku-metrics-agent.jar ${JAVA_OPTS:-}"
+  else
+    export JAVA_TOOL_OPTIONS="-javaagent:${HOME}/.heroku/bin/heroku-metrics-agent.jar ${JAVA_TOOL_OPTIONS:-}"
+  fi
+fi
