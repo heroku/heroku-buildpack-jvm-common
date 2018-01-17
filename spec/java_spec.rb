85a4cd4fad843f8a7534f14d59e25bdf302f2847
Joe Kutner <jpkutner@gmail.com>
2018-01-17 10:26:02 -0600

    Updated spec jdk versions

--- spec/java_spec.rb
+++ spec/java_spec.rb
@@ -10,7 +10,7 @@ describe "Java" do
       "https://api.github.com/repos/heroku/heroku-buildpack-jvm-common/tarball/#{jvm_common_branch}")
   end
 
-  ["1.7", "1.8", "8", "1.9", "9", "9.0.0", "zulu-1.8.0_144", "openjdk-1.8.0_151", "openjdk-9.0.1"].each do |version|
+  ["1.7", "1.8", "8", "1.9", "9", "9.0.0", "zulu-1.8.0_144", "openjdk-1.8.0_162", "openjdk-9.0.4"].each do |version|
     context "a simple java app on jdk-#{version}" do
       let(:app) { Hatchet::Runner.new("java-servlets-sample",
         :buildpack_url => "https://github.com/heroku/heroku-buildpack-java") }
@@ -31,7 +31,7 @@ describe "Java" do
     end
   end
 
-  ["1.7", "1.8", "openjdk-1.8.0_144", "zulu-1.8.0_144", "openjdk-9.0.1"].each do |version|
+  ["1.7", "1.8", "openjdk-1.8.0_162", "zulu-1.8.0_144", "openjdk-9.0.4"].each do |version|
     context "jdk-overlay on #{version}" do
       let(:app) { Hatchet::Runner.new("java-overlay-test",
         :buildpack_url => "https://github.com/heroku/heroku-buildpack-java") }
