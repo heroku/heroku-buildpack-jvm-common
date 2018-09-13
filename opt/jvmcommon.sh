ea8906d73ce79a22108598a49041bd717a445a34
Joe Kutner <jpkutner@gmail.com>
2018-09-13 15:30:24 -0500

    Add UseContainerSupport to default options

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -3,24 +3,29 @@
 export JAVA_HOME="$HOME/.jdk"
 export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH"
 export PATH="$HOME/.heroku/bin:$JAVA_HOME/bin:$PATH"
-limit=$(ulimit -u)
-case $limit in
-256)   # 1X Dyno
-  default_java_mem_opts="-Xmx300m -Xss512k"
-  ;;
-512)   # 2X Dyno
-  default_java_mem_opts="-Xmx686m"
-  ;;
-16384) # IX Dyno
-  default_java_mem_opts="-Xmx2g"
-  ;;
-32768) # PX Dyno
-  default_java_mem_opts="-Xmx12g"
-  ;;
-*)
-  default_java_mem_opts="-Xmx300m -Xss512k"
-  ;;
-esac
+
+if cat "$HOME/.jdk/release" | grep -q '^JAVA_VERSION="1[0-1]'; then
+  default_java_mem_opts="-XX:+UseContainerSupport"
+else
+  limit=$(ulimit -u)
+  case $limit in
+  256)   # 1X Dyno
+    default_java_mem_opts="-Xmx300m -Xss512k"
+    ;;
+  512)   # 2X Dyno
+    default_java_mem_opts="-Xmx686m"
+    ;;
+  16384) # IX Dyno
+    default_java_mem_opts="-Xmx2g"
+    ;;
+  32768) # PX Dyno
+    default_java_mem_opts="-Xmx12g"
+    ;;
+  *)
+    default_java_mem_opts="-Xmx300m -Xss512k"
+    ;;
+  esac
+fi
 
 if echo "${JAVA_OPTS:-}" | grep -q "\-Xmx"; then
   export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
