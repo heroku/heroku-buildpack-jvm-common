126270ffbf6929473b0e573a6cdc0e998270faf9
Joe Kutner <jpkutner@gmail.com>
2018-10-02 09:38:41 -0500

    Remove leading whitespace from default java opts

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -27,7 +27,7 @@ export PATH="$HOME/.heroku/bin:$JAVA_HOME/bin:$PATH"
 if cat "$HOME/.jdk/release" | grep -q '^JAVA_VERSION="1[0-1]'; then
   default_java_mem_opts="$(calculate_java_memory_opts "-XX:+UseContainerSupport")"
 else
-  default_java_mem_opts="$(calculate_java_memory_opts)"
+  default_java_mem_opts="$(calculate_java_memory_opts | sed 's/^ //')"
 fi
 
 if echo "${JAVA_OPTS:-}" | grep -q "\-Xmx"; then
