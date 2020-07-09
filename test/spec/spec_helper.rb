182130fc654af96ce60f5aa4ac9c9471f81819dc
Manuel Fuchs <malax@malax.de>
2020-07-09 16:39:54 +0200

    Update hatchet

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -68,10 +68,3 @@ def write_sys_props(d, props)
     `git add system.properties && git commit -m "setting jdk version"`
   end
 end
-
-ReplRunner.register_commands(:console)  do |config|
-  config.terminate_command "exit"          # the command you use to end the 'rails console'
-  config.startup_timeout 60                # seconds to boot
-  config.return_char "\n"                  # the character that submits the command
-  config.sync_stdout "STDOUT.sync = true"  # force REPL to not buffer standard out
-end
