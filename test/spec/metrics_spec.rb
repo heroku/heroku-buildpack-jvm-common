b1e888006b3b289cb299ce7f7f19def74db539cd
schneems <richard.schneeman+foo@gmail.com>
2020-07-16 09:43:28 -0500

    Update metrics spec to run in parallel

--- test/spec/metrics_spec.rb
+++ test/spec/metrics_spec.rb
@@ -1,25 +1,25 @@
 require_relative 'spec_helper'
 
 describe "JVM Metrics" do
-
-  before(:each) do
-    set_java_version(app.directory, jdk_version)
-    app.setup!
-    app.set_config(
-      "JVM_COMMON_BUILDPACK" =>
-      "https://api.github.com/repos/heroku/heroku-buildpack-jvm-common/tarball/#{jvm_common_branch}")
-    `heroku features:enable runtime-heroku-metrics --app #{app.name}`
-  end
-
-  ["1.7", "1.8", "11", "14"].each do |version|
-    context "a simple java app on jdk-#{version}" do
-      let(:app) { new_app_with_defaults("java-servlets-sample",
-        :buildpack_url => "https://github.com/heroku/heroku-buildpack-java") }
-      let(:jdk_version) { version }
+  ["1.7", "1.8", "11", "14"].each do |jdk_version|
+    context "a simple java app on jdk-#{jdk_version}" do
       it "should deploy" do
-        app.deploy do |app|
-          expect(app.output).to include("BUILD SUCCESS")
-          expect(successful_body(app)).to eq("Hello from Java!")
+        Hatchet::Runner.new(
+          "java-servlets-sample",
+          labs: "runtime-heroku-metrics",
+          buildpacks: ["https://github.com/heroku/heroku-buildpack-java"],
+          stack:  ENV["HEROKU_TEST_STACK"],
+          config: {
+            "JVM_COMMON_BUILDPACK" => "https://api.github.com/repos/heroku/heroku-buildpack-jvm-common/tarball/#{jvm_common_branch}"
+          }
+        ).tap do |app|
+          app.before_deploy do
+            set_java_version(Dir.pwd, jdk_version)
+          end
+          app.deploy do
+            expect(app.output).to include("BUILD SUCCESS")
+            expect(successful_body(app)).to eq("Hello from Java!")
+          end
         end
       end
     end
