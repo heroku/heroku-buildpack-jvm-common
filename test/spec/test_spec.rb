6066ebc0d12392cacff2c7121a3bad83bfcf704d
Lillian Zhang <zhang.l@salesforce.com>
2020-10-19 15:14:19 -0700

    use korvan

--- test/spec/test_spec.rb
+++ test/spec/test_spec.rb
@@ -2,16 +2,8 @@ require_relative "spec_helper"
 
 describe "A Java application" do
 
-  it "works with the getting started guide" do
-    Hatchet::Runner.new("java-getting-started").tap do |app|
-      app.deploy do
-        #deploy works
-      end
-    end
-  end
-
   it "checks for bad version" do
-    Hatchet::Runner.new("java-getting-started", allow_failure: true).tap do |app|
+    Hatchet::Runner.new("korvan", allow_failure: true).tap do |app|
       app.before_deploy do
         set_java_version(Dir.pwd, "14.badversion")
       end
@@ -26,7 +18,7 @@ describe "A Java application" do
       :default,
       "https://github.com/sharpstone/force_absolute_paths_buildpack"
     ]
-    Hatchet::Runner.new("java-getting-started", buildpacks: buildpacks).tap do |app|
+    Hatchet::Runner.new("korvan", buildpacks: buildpacks).tap do |app|
       app.deploy do
         #deploy works
       end
@@ -34,7 +26,7 @@ describe "A Java application" do
   end
 
   it "uses the cache with ci" do
-    Hatchet::Runner.new("java-getting-started").run_ci do |test_run|
+    Hatchet::Runner.new("korvan").run_ci do |test_run|
       expect(test_run.output).to include("Downloading from central")
       test_run.run_again
       expect(test_run.output).to_not include("Downloading from central")
