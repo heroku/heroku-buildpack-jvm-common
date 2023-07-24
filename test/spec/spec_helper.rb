750dcba1ef0fa0480d7b1c6a254500511461bfc5
Manuel Fuchs <manuel.fuchs@salesforce.com>
2023-07-24 11:14:31 +0200

    Fetch app url via Heroku API in CI (#270)

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -44,7 +44,7 @@ end
 def successful_body(app, options = {})
   retry_limit = options[:retry_limit] || 50
   path = options[:path] ? "/#{options[:path]}" : ''
-  Excon.get("http://#{app.name}.herokuapp.com#{path}", :idempotent => true, :expects => 200, :retry_limit => retry_limit).body
+  Excon.get("#{app.platform_api.app.info(app.name).fetch("web_url")}#{path}", :idempotent => true, :expects => 200, :retry_limit => retry_limit).body
 end
 
 def create_file_with_size_in(size, dir)
