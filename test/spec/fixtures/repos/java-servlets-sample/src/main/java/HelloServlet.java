58e773151dd978b77b052df88a4c95e31078a8b5
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-03-22 11:45:48 +0100

    Update java-servlets-sample

---
+++ test/spec/fixtures/repos/java-servlets-sample/src/main/java/HelloServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        resp.getWriter().print("Hello from Java!");
+    }
+}
