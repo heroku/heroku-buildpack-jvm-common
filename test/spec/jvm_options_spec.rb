ba933a57abb19e0230ba47eadac55c9290c6218b
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-01-23 16:14:03 +0100

    Correct Rubocop offenses

--- test/spec/jvm_options_spec.rb
+++ test/spec/jvm_options_spec.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 require_relative 'spec_helper'
 
-describe "Java" do
-  expected_max_ram={
-    "Basic": 536870912,
-    "Standard-1X": 536870912,
-    "Standard-2X": 1073741824,
-    "Performance-M": 2684354560,
-    "Performance-L": 15032385536,
-    "Performance-L-RAM": 32212254720,
-    "Performance-XL": 66571993088,
-    "Performance-2XL": 135291469824
+describe 'Java' do
+  expected_max_ram = {
+    Basic: 536_870_912,
+    'Standard-1X': 536_870_912,
+    'Standard-2X': 1_073_741_824,
+    'Performance-M': 2_684_354_560,
+    'Performance-L': 15_032_385_536,
+    'Performance-L-RAM': 32_212_254_720,
+    'Performance-XL': 66_571_993_088,
+    'Performance-2XL': 135_291_469_824,
   }
 
   expected_max_ram.each do |size, bytes|
-    context "JVM options for #{size}" do
-      it "should have the correct MaxRAM JVM option set" do
-        new_default_hatchet_runner("jvmoptions").tap do |app|
+    context "when JVM options for #{size}" do
+      it 'has the correct MaxRAM JVM option set' do
+        new_default_hatchet_runner('jvmoptions').tap do |app|
           app.deploy do
-            app.platform_api.formation.update(app.name, "web", {"size" => size})
+            app.platform_api.formation.update(app.name, 'web', { 'size' => size })
             expect(successful_body(app)).to eq("MaxRAM=#{bytes}")
           end
         end
--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'rspec/core'
 require 'hatchet'
 require 'fileutils'
-require 'hatchet'
 require 'rspec/retry'
 require 'date'
 
@@ -20,23 +21,23 @@ RSpec.configure do |config|
   end
 end
 
-def new_default_hatchet_runner(*args, **kwargs)
-  kwargs[:stack] ||= ENV["DEFAULT_APP_STACK"]
+def new_default_hatchet_runner(*, **kwargs)
+  kwargs[:stack] ||= ENV.fetch('DEFAULT_APP_STACK', nil)
   kwargs[:config] ||= {}
 
-  ENV.keys.each do |key|
-    if key.start_with?("DEFAULT_APP_CONFIG_")
-      kwargs[:config][key.delete_prefix("DEFAULT_APP_CONFIG_")] ||= ENV[key]
+  ENV.each_key do |key|
+    if key.start_with?('DEFAULT_APP_CONFIG_')
+      kwargs[:config][key.delete_prefix('DEFAULT_APP_CONFIG_')] ||= ENV.fetch(key, nil)
     end
   end
 
-  Hatchet::Runner.new(*args, **kwargs)
+  Hatchet::Runner.new(*, **kwargs)
 end
 
 def add_database(app, heroku)
   Hatchet::RETRIES.times.retry do
     heroku.post_addon(app.name, 'heroku-postgresql')
-    _, value = heroku.get_config_vars(app.name).body.detect {|key, value| key.match(/HEROKU_POSTGRESQL_[A-Z]+_URL/) }
+    _, value = heroku.get_config_vars(app.name).body.detect { |key, _value| key.match(/HEROKU_POSTGRESQL_[A-Z]+_URL/) }
     heroku.put_config_vars(app.name, 'DATABASE_URL' => value)
   end
 end
@@ -44,21 +45,22 @@ end
 def successful_body(app, options = {})
   retry_limit = options[:retry_limit] || 50
   path = options[:path] ? "/#{options[:path]}" : ''
-  Excon.get("#{app.platform_api.app.info(app.name).fetch("web_url")}#{path}", :idempotent => true, :expects => 200, :retry_limit => retry_limit).body
+  Excon.get("#{app.platform_api.app.info(app.name).fetch('web_url')}#{path}", idempotent: true, expects: 200,
+                                                                              retry_limit: retry_limit).body
 end
 
 def create_file_with_size_in(size, dir)
   name = File.join(dir, SecureRandom.hex(16))
-  File.open(name, 'w') {|f| f.print([ 1 ].pack("C") * size) }
+  File.open(name, 'w') { |f| f.print([1].pack('C') * size) }
   Pathname.new name
 end
 
-def set_java_version(d, v)
-  write_sys_props d, "java.runtime.version=#{v}"
+def set_java_version(directory, version)
+  write_sys_props directory, "java.runtime.version=#{version}"
 end
 
-def write_sys_props(d, props)
-  Dir.chdir(d) do
+def write_sys_props(directory, props)
+  Dir.chdir(directory) do
     `rm -f system.properties`
     File.open('system.properties', 'w') do |f|
       f.puts props
