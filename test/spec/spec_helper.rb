525f90dcda25eb57dd0ac500f6da064d9ec09f5d
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-02-03 14:38:27 +0100

    Remove unused helper functions

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -23,38 +23,11 @@ RSpec.configure do |config|
   config.verbose_retry = true
 end
 
-def new_default_hatchet_runner(*, **kwargs)
-  kwargs[:stack] ||= ENV.fetch('DEFAULT_APP_STACK', nil)
-  kwargs[:config] ||= {}
-
-  ENV.each_key do |key|
-    if key.start_with?('DEFAULT_APP_CONFIG_')
-      kwargs[:config][key.delete_prefix('DEFAULT_APP_CONFIG_')] ||= ENV.fetch(key, nil)
-    end
-  end
-
-  Hatchet::Runner.new(*, **kwargs)
-end
-
-def add_database(app, heroku)
-  Hatchet::RETRIES.times.retry do
-    heroku.post_addon(app.name, 'heroku-postgresql')
-    _, value = heroku.get_config_vars(app.name).body.detect { |key, _value| key.match(/HEROKU_POSTGRESQL_[A-Z]+_URL/) }
-    heroku.put_config_vars(app.name, 'DATABASE_URL' => value)
-  end
-end
-
 def successful_body(app, options = {})
   retry_limit = options[:retry_limit] || 50
   path = options[:path] ? "/#{options[:path]}" : ''
   Excon.get("#{app.platform_api.app.info(app.name).fetch('web_url')}#{path}", idempotent: true, expects: 200,
                                                                               retry_limit: retry_limit).body
-end
-
-def create_file_with_size_in(size, dir)
-  name = File.join(dir, SecureRandom.hex(16))
-  File.open(name, 'w') { |f| f.print([1].pack('C') * size) }
-  Pathname.new name
 end
 
 def set_java_version(directory, version)
