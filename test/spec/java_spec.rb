90e774476421521c252a9240e2fab490d316f40e
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-02-18 12:40:38 +0100

    Enhance provided JDK test (#338)

--- test/spec/java_spec.rb
+++ test/spec/java_spec.rb
@@ -186,4 +186,41 @@ RSpec.describe 'Java installation' do
       end
     end
   end
+
+  context 'when a JDK is already present in .jdk' do
+    it 'skips installing OpenJDK and outputs the correct message' do
+      app = Hatchet::Runner.new('empty')
+
+      app.before_deploy do
+        # Create a system.properties file to ensure the contents
+        # are ignored when a custom JDK is present.
+        set_java_version(Dir.pwd, '21')
+
+        # Install Amazon Corretto to the .jdk directory of the application
+        url = 'https://corretto.aws/downloads/resources/21.0.6.7.1/amazon-corretto-21.0.6.7.1-linux-x64.tar.gz'
+        tarball_name = 'corretto.tar.gz'
+        jdk_dir = '.jdk'
+
+        system("curl --silent -o #{tarball_name} --fail '#{url}'")
+        system("mkdir #{jdk_dir}")
+        system("tar --strip-components=1 -C #{jdk_dir} -xzf #{tarball_name}")
+      end
+
+      app.deploy do
+        expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX, Regexp::MULTILINE))
+          remote: -----> JVM Common app detected
+          remote: -----> Using provided JDK
+        REGEX
+
+        # Historically, the buildpack has not installed anything into .profile.d nor installed the metrics agent
+        # when a provided JDK is used. This might not have been intentional back then, but we test this behaviour
+        # here to ensure a change to that behaviour is noticed and intentional.
+        FILE_MD5_HASHES.each_key do |file_path|
+          expect(app.run("md5sum #{file_path}")).to include('No such file or directory')
+        end
+
+        expect(app.run('.jdk/bin/java -version')).to include('OpenJDK Runtime Environment Corretto-21.0.6.7.1')
+      end
+    end
+  end
 end
