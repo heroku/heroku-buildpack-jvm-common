240f6a317e3fd247f63a42edc3b25e8a02c98031
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-03-12 15:16:50 +0100

    Expand JVM options tests (#350)

--- test/spec/fixtures/repos/jvmoptions/src/main/java/com/heroku/jvmoptions/JvmOptionsController.java
+++ test/spec/fixtures/repos/jvmoptions/src/main/java/com/heroku/jvmoptions/JvmOptionsController.java
@@ -1,24 +1,35 @@
 package com.heroku.jvmoptions;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.sun.management.HotSpotDiagnosticMXBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestController
 public class JvmOptionsController {
     @GetMapping("/")
-    public String index() {
+    public JsonObject index(@RequestParam(value = "vmoptions", defaultValue = "") String vmOptions, @Autowired Gson gson) {
         var bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
-        var options = new String[]{"MaxRAM"};
 
-        var result = new StringBuilder();
+        var response = new JsonObject();
+
+        var options = Arrays.stream(vmOptions.split(","))
+                .map(String::trim)
+                .filter(option -> !option.isEmpty())
+                .toList();
+
         for (var option : options) {
-            result.append(option);
-            result.append("=");
-            result.append(bean.getVMOption(option).getValue());
+            response.add(option, new JsonPrimitive(bean.getVMOption(option).getValue()));
         }
 
-        return result.toString();
+        return response;
     }
 }
