ab87726742d9fdc64c25450250785008989ede41
Josh W Lewis <josh.w.lewis@gmail.com>
2025-08-04 09:55:29 -0500

    Don't log JAVA_TOOL_OPTIONS message on mcp* process types

--- opt/jvmcommon.sh
+++ opt/jvmcommon.sh
@@ -60,8 +60,11 @@ jvm_options="$(jvm_options)"
 export JAVA_OPTS="${jvm_options}${JAVA_OPTS:+" "}${JAVA_OPTS:-}"
 
 if ! [[ "${DYNO}" =~ ^run\..*$ ]]; then
-	# Redirecting to stderr to avoid polluting the application's stdout stream. This is especially important for
-	# MCP servers using the stdio transport: https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#stdio
-	echo "Setting JAVA_TOOL_OPTIONS defaults based on dyno size. Custom settings will override them." >&2
+	# Avoid logging this message on MCP servers to prevent polluting the application's output.
+	# This is especially important for MCP servers using the stdio transport: 
+	# https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#stdio
+	if ! [[ "${DYNO}" =~ ^mcp.*$ ]]; then
+		echo "Setting JAVA_TOOL_OPTIONS defaults based on dyno size. Custom settings will override them." >&2
+	fi
 	export JAVA_TOOL_OPTIONS="${jvm_options}${JAVA_TOOL_OPTIONS:+" "}${JAVA_TOOL_OPTIONS:-}"
 fi
