5737f19752418e67483491c55ec9afc951f14f84
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-02 15:32:25 +0200

    Fix `java_properties::get` error handling (#386)

--- lib/java_properties.sh
+++ lib/java_properties.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
+# This is technically redundant, since all consumers of this lib will have enabled these,
+# however, it helps Shellcheck realise the options under which these functions will run.
+# Temporarily commented out since not all downstream buildpacks that are sourcing the files of
+# this buildpack work with these settings yet.
+# set -euo pipefail
+
 # Reads the value of a key from a Java properties file
 #
 # ```
 # java_properties:get "system.properties" "java.runtime.version"
 # ```
-java_properties::get() {
+function java_properties::get() {
 	local file=${1:?}
 	local key=${2:?}
 
-	if [ -f "${file}" ]; then
-		local escaped_key
-		escaped_key="${key//\./\\.}"
+	local escaped_key
+	escaped_key="${key//\./\\.}"
 
-		grep -E "^${escaped_key}[[:space:]=]+" "${file}" |
-			sed -E -e "s/${escaped_key}([\ \t]*=[\ \t]*|[\ \t]+)([_A-Za-z0-9\.-]*).*/\2/g"
-	else
-		echo ""
-	fi
+	# Allow grep to fail (via `true`) for handling the case where the file or key is not found.
+	local grep_result
+	grep_result=$(grep -E "^${escaped_key}[[:space:]=]+" "${file}" 2>/dev/null || true)
+
+	echo "${grep_result}" | sed -E -e "s/${escaped_key}([\ \t]*=[\ \t]*|[\ \t]+)([_A-Za-z0-9\.-]*).*/\2/g"
 }
